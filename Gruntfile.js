74f27f670bda1568a409defcd3e0ac16f66e2423
Jesper Oskarsson <jesosk@gmail.com>
2015-03-02 00:45:43 +0100

    Added CC0 dedication to minified file

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,17 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
             options: {
+                banner: '\n\
+// publicdash.js - Javascript functional utilities in the public domain.\n\
+// Written in 2015 by Jesper Oskarsson jesosk@gmail.com\n\
+//\n\
+// To the extent possible under law, the author(s) have dedicated all copyright\n\
+// and related and neighboring rights to this software to the public domain worldwide.\n\
+// This software is distributed without any warranty.\n\
+//\n\
+// You should have received a copy of the CC0 Public Domain Dedication along with this software.\n\
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.\n\n\
+'
             },
             build: {
                 src: 'source/publicdash.js',
